3b6d9bb38c7f479d8f23a1c3704e2d25a860f273
Alan Quinones <Alan_QG@outlook.com>
2026-02-22 22:22:52 -0700

    feat: Proyecto Terminado JavaScript

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,99 @@
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
+import { useState, useEffect } from "react";
+import { db } from "./data/db";
+
 function App() {
+  // useEffect(() => {
+  //   console.log("Componente Listo o escuchando por Auth")
+  // }, [auth])
+
+  // [] -> Aqui dentro se agregan las dependencias o sea, si queremos que la funcion se ejecute cada que haya cambios en el carrito pondriamos
+  // [cart] -> puede tener multiples dependencias o ninguna
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+  const [data, setData] = useState([]);
+  const [cart, setCart] = useState(initialCart);
+  const MAX_ITEMS = 5
+  const MIN_ITEMS = 1
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
+
+  function addToCart(item) {
+    const itemExist = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExist >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemExist].quantity++;
+      setCart(updatedCart);
+      console.log("Ya existe");
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
+
+  function removeFromCart(id) {
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item;
+    });
+    setCart(updatedCart)
+  }
+
+  function decreaseQuantity(id){
+    const updatedCart = cart.map((item) => {
+      if(item.id === id && item.quantity > MIN_ITEMS) { 
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item;
+    })
+    setCart(updatedCart)
+  }
+  useEffect(() => {
+    setData(db);
+  }, []);
+
+  function clearCart() {
+    setCart([])
+  }
+
   return (
     <>
-    <Header />
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
+      />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-          <Guitar/>
-
-          
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
+          ))}
         </div>
       </main>
 
